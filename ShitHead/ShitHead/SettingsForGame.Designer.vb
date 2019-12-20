d9823f1a3dbac34cf8f9d6519e2ed1b1c52d3efe
jjarhead24 <jarexjared@gmail.com>
2019-12-20 23:08:38 +0000

    poo poo
    bwah

--- ShitHead/ShitHead/SettingsForGame.Designer.vb
+++ ShitHead/ShitHead/SettingsForGame.Designer.vb
@@ -41,10 +41,11 @@ Partial Class SettingsForGame
         Me.PayIn.AutoSize = False
         Me.PayIn.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
         Me.PayIn.LargeChange = 10
-        Me.PayIn.Location = New System.Drawing.Point(12, 145)
+        Me.PayIn.Location = New System.Drawing.Point(16, 178)
+        Me.PayIn.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.PayIn.Maximum = 100
         Me.PayIn.Name = "PayIn"
-        Me.PayIn.Size = New System.Drawing.Size(664, 39)
+        Me.PayIn.Size = New System.Drawing.Size(885, 48)
         Me.PayIn.SmallChange = 5
         Me.PayIn.TabIndex = 0
         Me.PayIn.TickFrequency = 10
@@ -53,11 +54,13 @@ Partial Class SettingsForGame
         'BotCount
         '
         Me.BotCount.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
-        Me.BotCount.Location = New System.Drawing.Point(12, 41)
+        Me.BotCount.LargeChange = 1
+        Me.BotCount.Location = New System.Drawing.Point(16, 50)
+        Me.BotCount.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.BotCount.Maximum = 3
         Me.BotCount.Minimum = 1
         Me.BotCount.Name = "BotCount"
-        Me.BotCount.Size = New System.Drawing.Size(664, 45)
+        Me.BotCount.Size = New System.Drawing.Size(885, 56)
         Me.BotCount.TabIndex = 1
         Me.BotCount.TickStyle = System.Windows.Forms.TickStyle.Both
         Me.BotCount.Value = 1
@@ -68,9 +71,10 @@ Partial Class SettingsForGame
         Me.Label1.BackColor = System.Drawing.Color.Transparent
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.Yellow
-        Me.Label1.Location = New System.Drawing.Point(184, 14)
+        Me.Label1.Location = New System.Drawing.Point(245, 17)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(380, 24)
+        Me.Label1.Size = New System.Drawing.Size(482, 29)
         Me.Label1.TabIndex = 2
         Me.Label1.Text = "How many bots do you want to play against?"
         '
@@ -80,27 +84,30 @@ Partial Class SettingsForGame
         Me.Label2.BackColor = System.Drawing.Color.Transparent
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.ForeColor = System.Drawing.Color.Yellow
-        Me.Label2.Location = New System.Drawing.Point(163, 118)
+        Me.Label2.Location = New System.Drawing.Point(217, 145)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(428, 24)
+        Me.Label2.Size = New System.Drawing.Size(542, 29)
         Me.Label2.TabIndex = 3
         Me.Label2.Text = "How many bills do you want to pay into the game?"
         '
         'BotNum
         '
         Me.BotNum.AutoSize = True
-        Me.BotNum.Location = New System.Drawing.Point(697, 55)
+        Me.BotNum.Location = New System.Drawing.Point(929, 68)
+        Me.BotNum.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.BotNum.Name = "BotNum"
-        Me.BotNum.Size = New System.Drawing.Size(39, 13)
+        Me.BotNum.Size = New System.Drawing.Size(51, 17)
         Me.BotNum.TabIndex = 4
         Me.BotNum.Text = "Label3"
         '
         'PayInNum
         '
         Me.PayInNum.AutoSize = True
-        Me.PayInNum.Location = New System.Drawing.Point(697, 156)
+        Me.PayInNum.Location = New System.Drawing.Point(929, 192)
+        Me.PayInNum.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.PayInNum.Name = "PayInNum"
-        Me.PayInNum.Size = New System.Drawing.Size(39, 13)
+        Me.PayInNum.Size = New System.Drawing.Size(51, 17)
         Me.PayInNum.TabIndex = 5
         Me.PayInNum.Text = "Label4"
         '
@@ -109,9 +116,10 @@ Partial Class SettingsForGame
         Me.Confirm.BackColor = System.Drawing.Color.Transparent
         Me.Confirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Confirm.ForeColor = System.Drawing.Color.Yellow
-        Me.Confirm.Location = New System.Drawing.Point(700, 207)
+        Me.Confirm.Location = New System.Drawing.Point(933, 255)
+        Me.Confirm.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Confirm.Name = "Confirm"
-        Me.Confirm.Size = New System.Drawing.Size(88, 36)
+        Me.Confirm.Size = New System.Drawing.Size(117, 44)
         Me.Confirm.TabIndex = 6
         Me.Confirm.Text = "Confirm"
         Me.Confirm.UseVisualStyleBackColor = False
@@ -121,9 +129,10 @@ Partial Class SettingsForGame
         Me.SubmitBills.BackColor = System.Drawing.Color.Transparent
         Me.SubmitBills.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.SubmitBills.ForeColor = System.Drawing.Color.Yellow
-        Me.SubmitBills.Location = New System.Drawing.Point(118, 207)
+        Me.SubmitBills.Location = New System.Drawing.Point(157, 255)
+        Me.SubmitBills.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.SubmitBills.Name = "SubmitBills"
-        Me.SubmitBills.Size = New System.Drawing.Size(75, 23)
+        Me.SubmitBills.Size = New System.Drawing.Size(100, 28)
         Me.SubmitBills.TabIndex = 7
         Me.SubmitBills.Text = "Submit"
         Me.SubmitBills.UseVisualStyleBackColor = False
@@ -133,9 +142,10 @@ Partial Class SettingsForGame
         Me.CustomAmount.BackColor = System.Drawing.Color.Green
         Me.CustomAmount.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.CustomAmount.ForeColor = System.Drawing.Color.Yellow
-        Me.CustomAmount.Location = New System.Drawing.Point(12, 212)
+        Me.CustomAmount.Location = New System.Drawing.Point(16, 261)
+        Me.CustomAmount.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.CustomAmount.Name = "CustomAmount"
-        Me.CustomAmount.Size = New System.Drawing.Size(100, 13)
+        Me.CustomAmount.Size = New System.Drawing.Size(133, 15)
         Me.CustomAmount.TabIndex = 8
         Me.CustomAmount.Text = "Write number here"
         '
@@ -144,18 +154,19 @@ Partial Class SettingsForGame
         Me.Label3.AutoSize = True
         Me.Label3.BackColor = System.Drawing.Color.Transparent
         Me.Label3.ForeColor = System.Drawing.Color.Yellow
-        Me.Label3.Location = New System.Drawing.Point(12, 191)
+        Me.Label3.Location = New System.Drawing.Point(16, 235)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(137, 13)
+        Me.Label3.Size = New System.Drawing.Size(186, 17)
         Me.Label3.TabIndex = 9
         Me.Label3.Text = "or write your number in here"
         '
         'SettingsForGame
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.ShitHead.My.Resources.Resources.Background
-        Me.ClientSize = New System.Drawing.Size(800, 255)
+        Me.ClientSize = New System.Drawing.Size(1067, 314)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.CustomAmount)
         Me.Controls.Add(Me.SubmitBills)
@@ -166,6 +177,7 @@ Partial Class SettingsForGame
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.BotCount)
         Me.Controls.Add(Me.PayIn)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "SettingsForGame"
         Me.Text = "SettingsForGame"
         CType(Me.PayIn, System.ComponentModel.ISupportInitialize).EndInit()
